0dacb394b317c60533d31e4d2b19e419741d625f
agorafob <alexander.ptrsyn@gmail.com>
2023-09-19 21:11:48 +0300

    udemi project2 19/09/2023

---
+++ src/main/java/com/alik/project2/repositories/BooksRepository.java
@@ -0,0 +1,14 @@
+package com.alik.project2.repositories;
+
+import com.alik.project2.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Book,Integer> {
+    Optional<Book> findByName(String name);
+    List<Book> findByNameStartingWith(String letters);
+}
---
+++ src/main/java/com/alik/project2/repositories/PeopleRepository.java
@@ -0,0 +1,14 @@
+package com.alik.project2.repositories;
+
+import com.alik.project2.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person,Integer> {
+
+        Optional<Person> findByName(String name);
+
+}
